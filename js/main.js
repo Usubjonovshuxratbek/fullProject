db89b264cc6ff965eab9f06ff4acffc2d3d9da95
Usubjonovshuxratbek <usubjonovshuxratbek1@gmail.com>
2023-05-13 15:52:42 +0300

    finish

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let elBtn = document.querySelector('.btn-menu')
+let elNav = document.querySelector('.nav')
+let elNavInner = document.querySelector('.nav__inner')
+let elNavInnerLink = document.querySelector('.nav__inner-link')
+elNav.classList.add()
+elBtn.addEventListener('click', ()=>{
+    elNav.classList.toggle('navv')
+    elNavInner.classList.toggle('nav__innerr')
+    elNavInner.toggle.style.display = "none"
+})
